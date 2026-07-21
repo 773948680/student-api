ca4cc6f79840c625d6e04f55a9cce7998d0e5fc5
Alpha Diallo <alalpha394@gmail.com>
2026-07-21 16:15:19 +0000

    Test volontairement en echec pour exercice 6.2

--- src/test/java/sn/uvs/studentapi/service/StudentServiceTest.java
+++ src/test/java/sn/uvs/studentapi/service/StudentServiceTest.java
@@ -30,7 +30,7 @@ class StudentServiceTest {
 
         List<Student> result = service.findAll();
 
-        assertEquals(1, result.size());
+        assertEquals(1333, result.size());
         verify(repository).findAll();
     }
 
